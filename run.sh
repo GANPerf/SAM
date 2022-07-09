3275f455328e92cbe626ff05a70cbb510b6b08ed
Yangyang Shu <50609620+GANPerf@users.noreply.github.com>
2022-07-09 22:32:16 +0930

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,69 +1,69 @@
 
-#CUB200
-#200 category 
+## CUB200
+## 200 category 
 python src/main.py  --root ./CUB200 --batch_size 24 --logdir vis/ --gpu_id 0 --backbone resnet50 --label_ratio 10 --pretrained
 python src/main.py  --root ./CUB200 --batch_size 24 --logdir vis/ --gpu_id 0 --backbone resnet50 --label_ratio 15 --pretrained
 python src/main.py  --root ./CUB200 --batch_size 24 --logdir vis/ --gpu_id 1 --backbone resnet50 --label_ratio 30 --pretrained
 python src/main.py  --root ./CUB200 --batch_size 24 --logdir vis/ --gpu_id 2 --backbone resnet50 --label_ratio 50 --pretrained
 
-#100 category 
+## 100 category 
 python src/main.py  --root ./CUB200 --batch_size 24 --logdir vis/ --gpu_id 0 --backbone resnet50 --label_ratio 10200100 --pretrained
 python src/main.py  --root ./CUB200 --batch_size 24 --logdir vis/ --gpu_id 0 --backbone resnet50 --label_ratio 15200100 --pretrained
 python src/main.py  --root ./CUB200 --batch_size 24 --logdir vis/ --gpu_id 1 --backbone resnet50 --label_ratio 30200100 --pretrained
 python src/main.py  --root ./CUB200 --batch_size 24 --logdir vis/ --gpu_id 2 --backbone resnet50 --label_ratio 50200100 --pretrained
 
-#50 category 
+## 50 category 
 python src/main.py  --root ./CUB200 --batch_size 24 --logdir vis/ --gpu_id 0 --backbone resnet50 --label_ratio 1020050 --pretrained
 python src/main.py  --root ./CUB200 --batch_size 24 --logdir vis/ --gpu_id 0 --backbone resnet50 --label_ratio 1520050 --pretrained
 python src/main.py  --root ./CUB200 --batch_size 24 --logdir vis/ --gpu_id 1 --backbone resnet50 --label_ratio 3020050 --pretrained
 python src/main.py  --root ./CUB200 --batch_size 24 --logdir vis/ --gpu_id 2 --backbone resnet50 --label_ratio 5020050 --pretrained
 
-#30 category 
+## 30 category 
 python src/main.py  --root ./CUB200 --batch_size 24 --logdir vis/ --gpu_id 0 --backbone resnet50 --label_ratio 1020030 --pretrained
 python src/main.py  --root ./CUB200 --batch_size 24 --logdir vis/ --gpu_id 0 --backbone resnet50 --label_ratio 1520030 --pretrained
 python src/main.py  --root ./CUB200 --batch_size 24 --logdir vis/ --gpu_id 1 --backbone resnet50 --label_ratio 3020030 --pretrained
 python src/main.py  --root ./CUB200 --batch_size 24 --logdir vis/ --gpu_id 2 --backbone resnet50 --label_ratio 5020030 --pretrained
 
 
-#StanfordCars
-#196 category 
+## StanfordCars
+## 196 category 
 python src/main.py  --root ./StanfordCars --batch_size 24 --logdir vis/ --gpu_id 0 --backbone resnet50  --label_ratio 10 --pretrained
 python src/main.py  --root ./StanfordCars --batch_size 24 --logdir vis/ --gpu_id 0 --backbone resnet50  --label_ratio 15 --pretrained
 python src/main.py  --root ./StanfordCars --batch_size 24 --logdir vis/ --gpu_id 1 --backbone resnet50  --label_ratio 30 --pretrained
 python src/main.py  --root ./StanfordCars --batch_size 24 --logdir vis/ --gpu_id 2 --backbone resnet50  --label_ratio 50 --pretrained
 
-#100 category 
+## 100 category 
 python src/main.py  --root ./StanfordCars --batch_size 24 --logdir vis/ --gpu_id 0 --backbone resnet50  --label_ratio 10196100 --pretrained
 python src/main.py  --root ./StanfordCars --batch_size 24 --logdir vis/ --gpu_id 0 --backbone resnet50  --label_ratio 15196100 --pretrained
 python src/main.py  --root ./StanfordCars --batch_size 24 --logdir vis/ --gpu_id 1 --backbone resnet50  --label_ratio 30196100 --pretrained
 python src/main.py  --root ./StanfordCars --batch_size 24 --logdir vis/ --gpu_id 2 --backbone resnet50  --label_ratio 50196100 --pretrained
 
-#50 category 
+## 50 category 
 python src/main.py  --root ./StanfordCars --batch_size 24 --logdir vis/ --gpu_id 0 --backbone resnet50  --label_ratio 1019650 --pretrained
 python src/main.py  --root ./StanfordCars --batch_size 24 --logdir vis/ --gpu_id 0 --backbone resnet50  --label_ratio 1519650 --pretrained
 python src/main.py  --root ./StanfordCars --batch_size 24 --logdir vis/ --gpu_id 1 --backbone resnet50  --label_ratio 3019650 --pretrained
 python src/main.py  --root ./StanfordCars --batch_size 24 --logdir vis/ --gpu_id 2 --backbone resnet50  --label_ratio 5019650 --pretrained
 
-#30 category 
+## 30 category 
 python src/main.py  --root ./StanfordCars --batch_size 24 --logdir vis/ --gpu_id 0 --backbone resnet50  --label_ratio 1019630 --pretrained
 python src/main.py  --root ./StanfordCars --batch_size 24 --logdir vis/ --gpu_id 0 --backbone resnet50  --label_ratio 1519630 --pretrained
 python src/main.py  --root ./StanfordCars --batch_size 24 --logdir vis/ --gpu_id 1 --backbone resnet50  --label_ratio 3019630 --pretrained
 python src/main.py  --root ./StanfordCars --batch_size 24 --logdir vis/ --gpu_id 2 --backbone resnet50  --label_ratio 5019630 --pretrained
 
-#Aircraft
-#100 category
+## Aircraft
+## 100 category
 python src/main.py  --root ./Aircraft --batch_size 24 --logdir vis/ --gpu_id 0 --backbone resnet50 --label_ratio 10 --pretrained
 python src/main.py  --root ./Aircraft --batch_size 24 --logdir vis/ --gpu_id 0 --backbone resnet50 --label_ratio 15 --pretrained
 python src/main.py  --root ./Aircraft --batch_size 24 --logdir vis/ --gpu_id 1 --backbone resnet50 --label_ratio 30 --pretrained
 python src/main.py  --root ./Aircraft --batch_size 24 --logdir vis/ --gpu_id 2 --backbone resnet50 --label_ratio 50 --pretrained
 
-#50 category
+## 50 category
 python src/main.py  --root ./Aircraft --batch_size 24 --logdir vis/ --gpu_id 0 --backbone resnet50 --label_ratio 1010050 --pretrained
 python src/main.py  --root ./Aircraft --batch_size 24 --logdir vis/ --gpu_id 0 --backbone resnet50 --label_ratio 1510050 --pretrained
 python src/main.py  --root ./Aircraft --batch_size 24 --logdir vis/ --gpu_id 1 --backbone resnet50 --label_ratio 3010050 --pretrained
 python src/main.py  --root ./Aircraft --batch_size 24 --logdir vis/ --gpu_id 2 --backbone resnet50 --label_ratio 5010050 --pretrained
 
-#30 category
+## 30 category
 python src/main.py  --root ./Aircraft --batch_size 24 --logdir vis/ --gpu_id 0 --backbone resnet50 --label_ratio 1010030 --pretrained
 python src/main.py  --root ./Aircraft --batch_size 24 --logdir vis/ --gpu_id 0 --backbone resnet50 --label_ratio 1510030 --pretrained
 python src/main.py  --root ./Aircraft --batch_size 24 --logdir vis/ --gpu_id 1 --backbone resnet50 --label_ratio 3010030 --pretrained
