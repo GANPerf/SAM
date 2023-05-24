cce32eb713c30533cff6c6dfb85328c6ccbd2179
Yangyang Shu <50609620+GANPerf@users.noreply.github.com>
2023-05-24 19:28:58 +0930

    Update and rename main.py to main_bp_gradcam.py

--- src/main_bp_gradcam.py
+++ src/main_bp_gradcam.py
@@ -1,29 +1,23 @@
 import sys
 import os
+
 proj_dir = os.path.abspath(os.getcwd())
 print("proj_dir: ", proj_dir)
 sys.path.append(proj_dir)
 
-import argparse
 import random
 
 import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.autograd import Variable
 
 from models.classifier import Classifier
 from models.method import SAM
 from tensorboardX import SummaryWriter
-from models.resnet import resnet18, resnet34, resnet50, resnet152, resnet101, ResBase, FeatB
+from models.resnet import ResBase, FeatB
 from src.utils import load_network, load_data
 
-#from src.CompactBilinearPooling import CompactBilinearPooling
-from compact_bilinear_pooling import CountSketch, CompactBilinearPooling
-
-from PIL import Image
-from torchvision import models
 import argparse
 from pytorch_grad_cam import GradCAM, \
     ScoreCAM, \
@@ -34,13 +28,10 @@ from pytorch_grad_cam import GradCAM, \
     EigenGradCAM, \
     LayerCAM, \
     FullGrad
-from pytorch_grad_cam import GuidedBackpropReLUModel
-from pytorch_grad_cam.utils.image import show_cam_on_image, \
-    deprocess_image, \
-    preprocess_image
 
 import torch.nn.functional as F
 import copy
+
 
 def test(loader, model, classifier, device):
     with torch.no_grad():
@@ -50,7 +41,7 @@ def test(loader, model, classifier, device):
         val_len = len(loader['test0'])
         iter_val = [iter(loader['test' + str(i)]) for i in range(10)]
         for _ in range(val_len):
-            data = [iter_val[j].next() for j in range(10)]
+            data = [next(iter_val[j]) for j in range(10)]
             inputs = [data[j][0] for j in range(10)]
             labels = data[0][1]
             for j in range(10):
@@ -58,8 +49,8 @@ def test(loader, model, classifier, device):
             labels = labels.to(device)
             outputs = []
             for j in range(10):
-                feat,_ = model.inference(inputs[j]) 
-                output,_ = classifier(feat.cuda())
+                feat, _ = model.inference(inputs[j])
+                output = classifier(feat.cuda())
                 outputs.append(output)
             outputs = sum(outputs)
             if start_test:
@@ -70,12 +61,12 @@ def test(loader, model, classifier, device):
                 all_outputs = torch.cat((all_outputs, outputs.data.float()), 0)
                 all_labels = torch.cat((all_labels, labels.data.float()), 0)
         _, predict = torch.max(all_outputs, 1)
-        
+
         accuracy = torch.sum(torch.squeeze(predict).float() == all_labels).item() / float(all_labels.size()[0])
     return accuracy
 
-def train(args, model, classifier, dataset_loaders, optimizer, scheduler, device=None, writer=None, model_path = None):
 
+def train(args, model, classifier, dataset_loaders, optimizer, scheduler, device=None, writer=None, model_path=None):
     len_labeled = len(dataset_loaders["train"])
     iter_labeled = iter(dataset_loaders["train"])
 
@@ -87,137 +78,144 @@ def train(args, model, classifier, dataset_loaders, optimizer, scheduler, device
     for iter_num in range(1, args.max_iter + 1):
         model.train(True)
         classifier.train(True)
-        optimizer.zero_grad()
         if iter_num % len_labeled == 0:
             iter_labeled = iter(dataset_loaders["train"])
-        
 
-        data_labeled = iter_labeled.next()
+        data_labeled = next(iter_labeled)
 
         img_labeled_q = data_labeled[0][0].to(device)
         label = data_labeled[1].to(device)
 
         feat_labeled, featmap_q, featcov16, bp_out_feat, network = model(img_labeled_q)
-	
-        out, cam_weight = classifier(feat_labeled.cuda()) #feat_labeled/bp_out_feat
 
-        #CAM
+        # TODO: Changed here
+        if args.use_bilinear and iter_num <= 800:
+            out = classifier(feat_labeled.cuda())
+        elif args.use_bilinear:
+            out = classifier(bp_out_feat.cuda())
+        else:
+            out = classifier(feat_labeled.cuda())
+        cam_weight = classifier.classifier_layer.weight
 
-        weight = cam_weight[label,:]
-        weight = weight.to(device)
-        weight = weight[:, :, None, None]
-        weight_cam = weight.repeat(1, 1, 7, 7)  
 
-        #----------------------------------------
-        
+        # ----------------------------------------
+
         classifier_loss = criterions['CrossEntropy'](out, label)
 
-        
         methods = \
-        {"gradcam": GradCAM,
-         "scorecam": ScoreCAM,
-         "gradcam++": GradCAMPlusPlus,
-         "ablationcam": AblationCAM,
-         "xgradcam": XGradCAM,
-         "eigencam": EigenCAM,
-         "eigengradcam": EigenGradCAM,
-         "layercam": LayerCAM,
-         "fullgrad": FullGrad}
+            {"gradcam": GradCAM,
+             "scorecam": ScoreCAM,
+             "gradcam++": GradCAMPlusPlus,
+             "ablationcam": AblationCAM,
+             "xgradcam": XGradCAM,
+             "eigencam": EigenCAM,
+             "eigengradcam": EigenGradCAM,
+             "layercam": LayerCAM,
+             "fullgrad": FullGrad}
 
-	
-	'''
-	# model for gradcam
-        model_f = ResBase().cuda()
-	model_f.conv1 = copy.deepcopy(network.conv1)
-	model_f.bn1 = copy.deepcopy(network.bn1)
-	model_f.relu = copy.deepcopy(network.relu)
-	model_f.maxpool = copy.deepcopy(network.maxpool)
-	model_f.layer1 = copy.deepcopy(network.layer1)
-	model_f.layer2 = copy.deepcopy(network.layer2)
-	model_f.layer3 = copy.deepcopy(network.layer3)
-	model_f.layer4 = copy.deepcopy(network.layer4)
-	
-	net_bilinear = FeatB().cuda()
-	net_bilinear.conv16 = copy.deepcopy(model.conv16)
-	net_bilinear.bn16 = copy.deepcopy(model.bn16)
-   
-	model_gradcam = nn.Sequential(model_f, net_bilinear, classifier) #classifier should be 16*2048->class_num
-	'''
-	
-	# for cam
-	for paramback, param_cam in zip(network.parameters(), model_cam.parameters()):
-            param_cam.data.copy_(paramback.data)
-	
-        target_layers = [model_cam.layer4[-1]]  #model_cam/model_f
+        # TODO: Changed here
+        if args.use_bilinear:
+            # model for gradcam
+            model_f = ResBase().cuda()
+            model_f.conv1 = copy.deepcopy(network.conv1)
+            model_f.bn1 = copy.deepcopy(network.bn1)
+            model_f.relu = copy.deepcopy(network.relu)
+            model_f.maxpool = copy.deepcopy(network.maxpool)
+            model_f.layer1 = copy.deepcopy(network.layer1)
+            model_f.layer2 = copy.deepcopy(network.layer2)
+            model_f.layer3 = copy.deepcopy(network.layer3)
+            model_f.layer4 = copy.deepcopy(network.layer4)
 
-        # along with line 172
-        '''
-        input_tensor_labeled_q = img_labeled_q
-        target_category = label
-        '''
+            net_bilinear = FeatB().cuda()
+            net_bilinear.conv16 = copy.deepcopy(model.conv16)
+            net_bilinear.bn16 = copy.deepcopy(model.bn16)
 
-        #GradCAM
-        cam_algorithm = methods[args.method] 
+            model_cam = nn.Sequential(model_f, net_bilinear, classifier)  # classifier should be 16*2048->class_num
+            target_layers = [model_f.layer4[-1]]
+        else:
+            model_cam = ResBase().cuda()
 
-        with cam_algorithm(model=model_cam,  #model_cam/model_gradcam for CAM/GradCAM
-                           target_layers=target_layers,
-                           use_cuda=args.use_cuda) as cam:
-        
-            # AblationCAM and ScoreCAM have batched implementations.
-            # You can override the internal batch size for faster computation.
-            cam.batch_size = 24
+            # for cam
+            for paramback, param_cam in zip(network.parameters(), model_cam.parameters()):
+                param_cam.data.copy_(paramback.data)
 
-            #for weight generated by gradcam
-            '''
-            _, weights_gradcam = cam(input_tensor=input_tensor_labeled_q,
-                                target_category=target_category,
+            target_layers = [model_cam.layer4[-1]]
+
+        # TODO: Changed here
+        if args.use_bilinear:
+            # GradCAM
+            cam_algorithm = methods[args.method]
+
+            with cam_algorithm(model=model_cam,
+                               target_layers=target_layers,
+                               use_cuda=args.use_cuda) as cam:
+
+                # AblationCAM and ScoreCAM have batched implementations.
+                # You can override the internal batch size for faster computation.
+                cam.batch_size = 24
+
+                # for weight generated by gradcam
+                _, weight = cam(input_tensor=img_labeled_q,
+                                target_category=label,
                                 aug_smooth=args.aug_smooth,
                                 eigen_smooth=args.eigen_smooth)
-            '''
+                weight: torch.Tensor = torch.from_numpy(weight).cuda()  # BxF
+                weight = weight[:, :, None, None]
+                weight_cam = weight.repeat(1, 1, 7, 7)
+        else:
+            # CAM
+            weight = cam_weight[label, :]
+            weight = weight.to(device)
+            weight = weight[:, :, None, None]
+            weight_cam = weight.repeat(1, 1, 7, 7)
 
+        # TODO: Changed here
+        featmapcam = featmap_q * weight_cam
+        featmapcam = torch.sum(featmapcam, dim=1)
 
-            # weight_gradcam/weight_cam
-            featmapcam = featmap_q*weight_cam
-            featmapcam = torch.sum(featmapcam, dim=1)
+        relu = nn.ReLU(inplace=True)
+        featmapcam = relu(featmapcam)
 
+        predictcam, _ = torch.max(featcov16, dim=1)
+        predict_cam = predictcam.to(device)
+        # temperature parameter in softmax
+        t = 0.4
+        featmapcam = featmapcam.view(featmapcam.size(0), -1)
+        featmapcam = (featmapcam / t).float()
+        featmapcam = featmapcam.detach()
+        predict_cam = predict_cam.view(predict_cam.size(0), -1)
+        predict_cam = (predict_cam / t).float()
 
-            relu = nn.ReLU(inplace=True)
-            featmapcam = relu(featmapcam)
-			
+        loss_cam_labeled_q = F.kl_div(predict_cam.softmax(dim=-1).log(), featmapcam.softmax(dim=-1),
+                                      reduction='sum')
 
-            predictcam, _ = torch.max(featcov16, dim=1)
-            predict_cam = predictcam.to(device)
-            #temperature parameter in softmax
-            t = 0.4
-            featmapcam = featmapcam.view(featmapcam.size(0), -1)
-            featmapcam = (featmapcam/t).float()
-            featmapcam = featmapcam.detach()
-            predict_cam = predict_cam.view(predict_cam.size(0),-1)
-            predict_cam = (predict_cam/t).float()
+        # TODO: Remove this...
+        if torch.isinf(loss_cam_labeled_q).any():
+            predicted_has_inf = torch.isinf(predict_cam.softmax(dim=-1).log()).any()
+            target_has_zeroes = (featmapcam.softmax(dim=-1) == 0).any()
+            print('Is Inf')
+        # print(iter_num, classifier_loss.item(), loss_cam_labeled_q.item())
 
+        total_loss = classifier_loss + 0.01 * loss_cam_labeled_q
 
-            loss_cam_labeled_q = F.kl_div(predict_cam.softmax(dim=-1).log(), featmapcam.softmax(dim=-1), reduction='sum')
-          
-       
-
-        total_loss = classifier_loss + 0.01*loss_cam_labeled_q
+        optimizer.zero_grad()
         total_loss.backward(retain_graph=True)
         optimizer.step()
         scheduler.step()
 
-
-        ## Calculate the training accuracy of current iteration
+        # Calculate the training accuracy of current iteration
         if iter_num % 100 == 0:
             _, predict = torch.max(out, 1)
             hit_num = (predict == label).sum().item()
             sample_num = predict.size(0)
             print("iter_num: {}; current acc: {}".format(iter_num, hit_num / float(sample_num)))
+            print("cam_loss: {}; classifier_loss: {}".format(loss_cam_labeled_q, classifier_loss))
 
-        ## Show Loss in TensorBoard
+        # Show Loss in TensorBoard
         writer.add_scalar('loss/cam_loss', loss_cam_labeled_q, iter_num)
         writer.add_scalar('loss/classifier_loss', classifier_loss, iter_num)
         writer.add_scalar('loss/total_loss', total_loss, iter_num)
-        
+
         if iter_num % args.test_interval == 1:
             model.eval()
             classifier.eval()
@@ -231,9 +229,10 @@ def train(args, model, classifier, dataset_loaders, optimizer, scheduler, device
                               'step': iter_num
                               }
 
-    print("best acc: %.4f" % (best_acc))
+    print("best acc: %.4f" % best_acc)
     torch.save(best_model, model_path)
     print("The best model has been saved in ", model_path)
+
 
 def read_config():
     parser = argparse.ArgumentParser()
@@ -245,6 +244,7 @@ def read_config():
     parser.add_argument('--seed', type=int, default='666666')
     parser.add_argument('--workers', type=int, default='4')
     parser.add_argument('--lr_ratio', type=float, default='10')
+    parser.add_argument('--use_bilinear', action='store_true', default=True, help='If True, Use SAM Bilinear')
     parser.add_argument('--backbone', type=str, default='resnet50')
     parser.add_argument('--weight_decay', type=float, default=0.0001)
     parser.add_argument('--momentum', type=float, default=0.9, help='the momentum hyperparameter')
@@ -252,7 +252,7 @@ def read_config():
     parser.add_argument('--class_num', type=int, default=200)
     parser.add_argument('--gpu_id', type=int, default=1)
     parser.add_argument('--max_iter', type=float, default=30000)
-    parser.add_argument('--test_interval', type=float, default=100)
+    parser.add_argument('--test_interval', type=float, default=1000)
     parser.add_argument("--pretrained", action="store_true", help="use the pre-trained model")
     parser.add_argument("--pretrained_path", type=str, default='~/.torch/models/moco_v2_800ep_pretrain.pth.tar')
 
@@ -268,8 +268,8 @@ def read_config():
     parser.add_argument(
         '--eigen_smooth',
         action='store_true',
-        help='Reduce noise by taking the first principle componenet'
-        'of cam_weights*activations')
+        help='Reduce noise by taking the first principle component'
+             'of cam_weights*activations')
     parser.add_argument('--method', type=str, default='gradcam',
                         choices=['gradcam', 'gradcam++',
                                  'scorecam', 'xgradcam',
@@ -277,7 +277,6 @@ def read_config():
                                  'eigengradcam', 'layercam', 'fullgrad'],
                         help='Can be gradcam/gradcam++/scorecam/xgradcam'
                              '/ablationcam/eigencam/eigengradcam/layercam')
-    
 
     configs = parser.parse_args()
     configs.use_cuda = configs.use_cuda and torch.cuda.is_available()
@@ -285,6 +284,7 @@ def read_config():
         print('Using GPU for acceleration')
     else:
         print('Using CPU for computation')
+
     return configs
 
 
@@ -313,43 +313,49 @@ def main():
     torch.cuda.set_device(args.gpu_id)
     device = torch.device('cuda:{}'.format(args.gpu_id) if torch.cuda.is_available() else 'cpu')
 
-
     model_name = "%s_%s_%s" % (args.backbone, os.path.basename(args.root), str(args.label_ratio))
 
-    logdir = os.path.join(args.logdir, model_name)
     method_name = 'SAM_logConfid'
     method_name += '_qdim' + str(args.projector_dim)
 
     method_name += '_seed' + str(args.seed)
 
     logdir = os.path.join(args.logdir, method_name)
+    logdir = f"{logdir}{model_name}"
     if not os.path.exists(logdir):
         os.makedirs(logdir)
     writer = SummaryWriter(logdir)
 
-    model_path = os.path.join(logdir, "%s_best.pkl" % (model_name))
+    model_path = os.path.join(logdir, "%s_best.pkl" % model_name)
 
     # Initialize model
     network, feature_dim = load_network(args.backbone)
-    model = SAM(network=network, backbone=args.backbone, projector_dim=args.projector_dim,
-                       class_num=args.class_num, pretrained=args.pretrained, pretrained_path=args.pretrained_path).to(device)
-    classifier = Classifier(2048, args.class_num).to(device)   #2048/num of bilinear 2048*16
+    model = SAM(network=network, backbone=args.backbone, use_bilinear=args.use_bilinear,
+                projector_dim=args.projector_dim,
+                class_num=args.class_num, pretrained=args.pretrained, pretrained_path=args.pretrained_path).to(device)
+
+    # TODO: Changed here
+    if args.use_bilinear:
+        classifier = Classifier(2048*1, args.class_num).to(device)  ##I have changed!!!
+    else:
+        classifier = Classifier(2048, args.class_num).to(device)
 
     print("backbone params: {:.2f}M".format(sum(p.numel() for p in model.parameters()) / 1e6 / 2))
     print("classifier params: {:.2f}M".format(sum(p.numel() for p in classifier.parameters()) / 1e6))
 
-    ## Define Optimizer
+    # Define Optimizer
     optimizer = optim.SGD([
         {'params': model.parameters()},
         {'params': classifier.parameters(), 'lr': args.lr * args.lr_ratio},
 
-    ], lr= args.lr, momentum=0.9, weight_decay=args.weight_decay, nesterov=True)
+    ], lr=args.lr, momentum=0.9, weight_decay=args.weight_decay, nesterov=True)
     milestones = [6000, 12000, 18000, 24000, 30000]
     scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones, gamma=0.1)
 
     # Train model
-    train(args, model, classifier, dataset_loaders, optimizer, scheduler, device=device, writer=writer, model_path=model_path)
+    train(args, model, classifier, dataset_loaders, optimizer, scheduler, device=device, writer=writer,
+          model_path=model_path)
 
-   
+
 if __name__ == '__main__':
     main()
