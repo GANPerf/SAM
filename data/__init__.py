f6f790d159c992b728dc7cc3bdc6078741391aab
Yangyang Shu <50609620+GANPerf@users.noreply.github.com>
2023-05-24 19:29:20 +0930

    Delete data directory

--- data/__init__.py
+++
@@ -1,6 +0,0 @@
-from .imagelist import ImageList
-from .aircrafts import Aircraft
-from .cub200 import CUB200
-from .stanford_cars import StanfordCars
-
-__all__ = ['ImageList', "Aircraft", "CUB200", "StanfordCars"]
